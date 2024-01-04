8fffbd3a2aedea6b3a3920bd45b0d80fb82a34a0
Ada Keita <mathildebrakstad@gmail.com>
2024-01-04 22:30:03 +0100

    adding frontpage about

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+<section className="frontpage-about">
+    <h1 className="frontpage-header">Welcome to CleanSlate</h1>
+    <p className="frontpage-about-text">
+
+        Welcome to CleanSweep, where we make household chores fun and fair! Track your tasks, see their value as if they were paid, and compare with your partner. Discover a balanced home life with our easy-to-use app, designed for teamwork and appreciation. With CleanSweep, every chore counts towards a harmonious 50/50 partnership. Join us in celebrating and equalizing the unseen efforts of home life!
+        Welcome to CleanSweep - the friendly app that brings a new perspective to household chores! Designed for two, CleanSweep turns the often-overlooked unpaid home tasks into something you can track and value. Here’s how it works: Simply log the time you spend on different chores, and voilà! The app calculates what that effort would be worth if it were a paid job.
+    </p>
+    <p className="frontpage-about-text">
+        It’s not just about numbers though; it’s about appreciation. You get to see your own stats and costs, and even compare them with your partner’s. Think of it as a fun and eye-opening way to acknowledge each other's contributions at home.
+
+        And hey, if things are a bit lopsided in the chore department, CleanSweep has a neat solution. You can easily balance things out by sending a little thank-you via Swish to your partner.
+    </p>
+    <p className="frontpage-about-text">
+        It's all about making sure everyone feels valued and keeping things fair and square at home.
+
+        Our goal? To nudge couples towards a happy, 50/50 balance in their domestic life. CleanSweep isn't just an app; it's your partner in fostering great conversations and a harmonious home. Let's make those unseen efforts seen and celebrated!</p>
+</section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,12 @@ const HomePage = () => {
                 <div className="frontpage-image-wrapper">
                     <img src={cleanSlateHome} alt="frontpage-img" />
                 </div>
+                <section className="frontpage-about">
+                    <p className="frontpage-about-text">
+                        Welcome to CleanSweep, where we make household chores fun and fair! Track your tasks, see their value as if they were paid, and compare with your partner. Discover a balanced home life with our easy-to-use app, designed for teamwork and appreciation.
+                        Join us in celebrating and equalizing the unseen efforts of home life!
+                    </p>
+                </section>
                 <div className="frontpage-btn-wrapper">
                     <button className="btn-rounded homepage-btns">Sign In</button>
                     <button className="btn-rounded homepage-btns">Sign Up</button>

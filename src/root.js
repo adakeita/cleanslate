690bdf657ee61e698883def093a96310c04db8df
Ada Keita <mathildebrakstad@gmail.com>
2024-01-15 02:29:42 +0100

    adding img and about-root

--- src/root.js
+++ src/root.js
@@ -5,6 +5,7 @@ import CompleteProfile from "./pages/CompleteProfile";
 import Dashboard from "./pages/Dashboard";
 import ProfilePage from "./pages/Profile";
 import LoginPage from "./pages/Login";
+import AboutPage from "./pages/About";
 import Root from "./App";
 
 const rootRoute = new RootRoute({
@@ -49,6 +50,12 @@ const loginRoute = new Route({
 	component: LoginPage,
 });
 
+const aboutRoute = new Route({
+	getParentRoute: () => rootRoute,
+	path: "/about",
+	component: AboutPage,
+});
+
 const routeTree = rootRoute.addChildren([
 	indexRoute,
 	registerRoute,
@@ -56,6 +63,7 @@ const routeTree = rootRoute.addChildren([
 	dashboardRoute,
 	loginRoute,
 	profileRoute,
+	aboutRoute,
 ]);
 
 export const router = new Router({ routeTree });

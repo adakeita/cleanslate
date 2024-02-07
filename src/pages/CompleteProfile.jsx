ca819b168939efdf3c2e4e85b6e840d1224b5e51
Ada Keita <mathildebrakstad@gmail.com>
2024-02-07 16:53:38 +0100

    I hope this works context

--- src/pages/CompleteProfile.jsx
+++ src/pages/CompleteProfile.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useUpdateBodyClass } from "../hooks/useUpdateBodyClass";
+import { useUI } from "../contexts/UIContext";
 import WelcomeUser from "../components/WelcomeUser";
 import UserDetails from "../components/UserDetails";
 import HouseholdDetails from "../components/HousholdDetails";
@@ -7,6 +8,14 @@ import HouseholdDetails from "../components/HousholdDetails";
 
 const CompleteProfile = () => {
     useUpdateBodyClass("/completeprofile");
+
+    const { setIsMenuVisible } = useUI();
+
+    useEffect(() => {
+      setIsMenuVisible(false);
+      return () => setIsMenuVisible(true);
+    }, [setIsMenuVisible]);
+  
 
     const [welcomeAcknowledged, setWelcomeAcknowledged] = useState(false);
     const [userDetailsCompleted, setUserDetailsCompleted] = useState(false);

5b9308de397c78779208f23a482b2a806001c772
Ada Keita <mathildebrakstad@gmail.com>
2024-04-01 13:46:08 +0200

    usecontext

--- src/pages/InvitePage.jsx
+++ src/pages/InvitePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { joinHouseholdUsingToken } from "../lib/api";
 import { UserDetailsContext } from "../contexts/UserDetailsContext";
